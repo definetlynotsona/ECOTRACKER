a1a4cf5daf2a2c0e1afac64debd07f8fd8dd21d4
definetlynotsona <168037307+definetlynotsona@users.noreply.github.com>
2024-05-10 15:43:26 +0800

    Add files via upload

---
+++ webesite/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+from flask_login import  login_required,  current_user
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+@login_required
+def home():
+    return render_template('index.html', user=current_user)
+
+@views.route('/about')
+@login_required
+def about():
+    return render_template('about.html', user=current_user)
+
+@views.route('/learn')
+@login_required
+def learn():
+    return render_template('learn.html', user=current_user)
+
+@views.route('/details')
+@login_required
+def details():
+    return render_template('details.html', user=current_user)
