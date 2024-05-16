0614b6a141e34ceb64d1a02ff1ac8e3eb6ac5862
definetlynotsona <168037307+definetlynotsona@users.noreply.github.com>
2024-05-16 19:23:46 +0800

    Update models.py

--- webesite/models.py
+++ webesite/models.py
@@ -7,3 +7,21 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
+
+class Pickup_Details(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)  # Make the name field nullable
+    address = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    pickup_point = db.Column(db.String(200), nullable=False)
+    quantity = db.Column(db.String(100), nullable=False)  # Changed to string as per your previous change
+    contact_no = db.Column(db.String(20), nullable=False)  # Adjust nullable based on your requirements
+    date = db.Column(db.String(20), nullable=False)
+    time_slot = db.Column(db.String(50), nullable=False)
+    
+class Donation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50))
+    variety = db.Column(db.String(50))
+    weight = db.Column(db.Float)
+    total_price = db.Column(db.String(50))
